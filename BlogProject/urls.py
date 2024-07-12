b3d48ff45168399e3557d3d6939f35dd02e135a7
Svitlana <svitlanagayduchyk76@gmail.com>
2024-07-12 13:49:27 +0300

    add views and urls for posts and comments

--- BlogProject/urls.py
+++ BlogProject/urls.py
@@ -1,7 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("blog_app.urls")),
+    path('accounts/login/$', views.auth_login, name='login'),
+    path('accounts/logout/$', views.auth_logout, name='logout', kwargs={'next_page': '/'}, ),
 ]
