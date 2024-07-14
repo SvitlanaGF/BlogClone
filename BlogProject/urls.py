9a7c5bbbbeab43333879d63be2e0a1e0c5c2cf03
Svitlana <svitlanagayduchyk76@gmail.com>
2024-07-14 10:10:33 +0300

    corrected the errors

--- BlogProject/urls.py
+++ BlogProject/urls.py
@@ -2,10 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
-
+# from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog_app.urls")),
-    path('accounts/login/$', views.auth_login, name='login'),
-    path('accounts/logout/$', views.auth_logout, name='logout', kwargs={'next_page': '/'}, ),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}, ),
 ]
